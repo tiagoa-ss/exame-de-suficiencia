2c5068bc20782bb2b3e8e8e2112e9e9b7f44b00b
tiagoa-ss <tiago201andre@gmail.com>
2023-09-28 21:58:15 -0300

    Added modal

---
+++ main.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", () => {
+  const modal = document.getElementById("menu-modal");
+
+  console.log(modal);
+
+  const btn = document.getElementById("menu");
+
+  const span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = () => {
+    modal.style.display = 'block';
+  };
+
+  span.onclick = () => {
+    modal.style.display = "none";
+  };
+
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
+);
